edbb56a927de6623bed609975eba11c3a50aae99
Diego Vieira <61810162+DiegoVSC42@users.noreply.github.com>
2020-11-29 17:45:14 -0300

    Add files via upload

---
+++ 5i.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE CURSO (id_curso INT PRIMARY KEY, 
+nome_curso VARCHAR(100) NOT NULL, 
+carga_horaria SMALLINT );
+
+CREATE TABLE TURMA (id_turma INT NOT NULL PRIMARY KEY,
+id_curso INT NOT NULL FOREIGN KEY,
+id_professor INT NOT NULL FOREIGN KEY,
+id_aluno INT NOT NULL FOREIGN KEY,
+data_matricula DATE NOT NULL);
+
+CREATE TABLE PROFESSOR(id_professor INT NOT NULL PRIMARY KEY,
+Nome_professor VARCHAR(100)	NOT NULL, 
+titulacao_professor CHAR(3));
+
+CREATE TABLE ALUNO(id_aluno INT NOT NULL PRIMARY KEY,
+nome_aluno VARCHAR(100) NOT NULL,
+email_aluno VARCHAR(100),
+fone_contato CHAR(14));
